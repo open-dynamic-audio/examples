58ef2f9a824f3796cf09f6080a1d16c89256e9ac
Kazuo256 <kazuo256@gmail.com>
2015-04-29 11:47:21 -0300

    Initializing ODA's system

--- src/main.cxx
+++ src/main.cxx
@@ -1,9 +1,19 @@
 
 #include <oda/oda.h>
+#include <oda/status.h>
+
+#include <cstdio>
 
 int main () {
   oda::dummy();
   oda::System sys;
+  oda::Status status = sys.start();
+  if (!status.ok()) {
+    std::printf("Error: %s\b", status.description().c_str());
+    return 1;
+  }
+  std::printf("Opened device %s\n", status.description().c_str());
+  sys.finish();
   return 0;
 }
 
