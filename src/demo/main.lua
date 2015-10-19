064c8fde749f0de01d21e93342f37b1343309e77
Kazuo256 <kazuo256@gmail.com>
2015-10-19 09:06:27 -0200

    LÖVE demo
    Add keyboard input events that push commands to loaded event:
    - 'return' key starts the event
    - 'tab' key initiates modulation

--- src/demo/main.lua
+++ src/demo/main.lua
@@ -2,25 +2,27 @@
 require 'defs'
 
 local oda = require 'oda'
+local ev
 
 function love.load ()
   oda.start()
   oda.registerPath("../patches") 
   oda.registerPath(ODA_PATCHES_PATH) 
-  local ev = oda.eventInstance "example"
+  ev = oda.eventInstance "example"
 end
 
 function love.keypressed (key)
   if key == 'escape' then
     love.event.push 'quit'
+  elseif key == 'return' then
+    ev:pushCommand('start')
+  elseif key == 'tab' then
+    ev:pushCommand('test')
   end
 end
-  
-local time = 0
 
 function love.update (dt)
   oda.tick()
-  time = time + dt
 end
 
 function love.quit ()
