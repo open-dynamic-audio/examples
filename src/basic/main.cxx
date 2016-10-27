7ec0ec98a3e01550dce1e286d46dfd1ea5040524
Kazuo256 <kazuo256@gmail.com>
2016-10-27 14:47:10 -0200

    [basic] Use real dt for steps

--- src/basic/main.cxx
+++ src/basic/main.cxx
@@ -2,65 +2,75 @@
 #include <oda/oda.h>
 
 #include <chrono>
+#include <iostream>
 #include <memory>
+#include <string>
 #include <thread>
-
-#include <cstdio>
+#include <vector>
 
 using std::chrono::high_resolution_clock;
 using std::chrono::milliseconds;
+using std::cout;
+using std::endl;
 using std::shared_ptr;
+using std::string;
 using std::this_thread::sleep_for;
+using std::vector;
 
 using Clock = high_resolution_clock;
 
 const auto ONE_SECOND = Clock::duration(milliseconds(1000));
 
-void gameTick (shared_ptr<oda::SoundtrackEvent> ev, double *seconds) {
-  auto sleep_time = Clock::duration(milliseconds(10 + rand()%16));
-  ev->pushCommand("step", static_cast<int>(*seconds));
-  sleep_for(sleep_time);
-  *seconds += 1.0*sleep_time.count()/ONE_SECOND.count();
-}
+void gameTick (double delta, shared_ptr<oda::SoundtrackEvent> ev);
 
 int main (int argc, char** argv) {
-
-  if (argc < 2) {
-    std::printf("%s: Please specify event. Available ones are:\n%s",
-                argv[0], "\texample\n\tbizarre\n\togg\n");
-    return 1;
+  string event_name = "basic";
+  if (argc >= 2) {
+    event_name = argv[1];
   }
 
   std::srand(std::time(0));
 
   oda::Engine engine;
-  oda::Status status = engine.start({"../patches", ODA_PATCHES_PATH});
-  if (!status.ok()) {
-    std::printf("Error: %s\n", status.description().c_str());
-    return 1;
+  {
+    oda::Status status = engine.start({"../patches", ODA_PATCHES_PATH});
+    if (!status.ok()) {
+      cout << "Error: " << status.description() << endl;
+      return 1;
+    }
+    cout << "Opened device " << status.description() << endl;
   }
-  std::printf("Opened device %s\n", status.description().c_str());
 
   {
     shared_ptr<oda::SoundtrackEvent> ev;
-
     {
-      std::string patch_input = argv[1];
-      oda::Status status = engine.eventInstance(patch_input, &ev);
+      oda::Status status = engine.eventInstance(event_name, &ev);
       if (!status.ok()) {
-        std::printf("Error: %s\n", status.description().c_str());
+        cout << "Error: " << status.description() << endl;
         engine.finish();
         return 1;
       }
     }
 
     double seconds = 0.0;
+    auto last = Clock::now();
     while (seconds < 10.0) {
-      gameTick(ev, &seconds);
-      engine.tick(seconds);
+      double delta = 1.0*(Clock::now() - last).count()/ONE_SECOND.count();
+      last = Clock::now();
+      gameTick(delta, ev);
+      engine.tick(delta);
+      seconds += delta;
     }
   }
 
   engine.finish();
   return 0;
+}
+
+void gameTick (double delta, shared_ptr<oda::SoundtrackEvent> ev) {
+  static double total = 0.0;
+  auto sleep_time = Clock::duration(milliseconds(16 + rand()%16));
+  total += delta;
+  ev->pushCommand("step", static_cast<int>(total));
+  sleep_for(sleep_time);
 }
